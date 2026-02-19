428c375eafd8b017a794bb368b9e99e98e6e019d
Mikhayla15 <mce45@pitt.edu>
2026-02-18 19:24:21 -0500

    added seashells, bridge, and sandcastle to foreground to create scene

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
+    public float speed = 2f; 
+
+    public float jumpHeight = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,44 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                // Get the current position of the player
+                Vector2 currentPosition = gameObject.transform.position;
+
+                // Get new position by adding to the x coordinate 
+                Vector2 newPos = new Vector2(currentPosition.x + Time.deltaTime * speed, currentPosition.y);
+
+                //update the player's position
+                gameObject.transform.position = newPos;
+            }
+
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                // Get the current position of the player
+                Vector2 currentPosition = gameObject.transform.position;
+
+                // Get new position by adding to the x coordinate 
+                Vector2 newPos = new Vector2(currentPosition.x - Time.deltaTime * speed, currentPosition.y);
+
+                //update the player's position
+                gameObject.transform.position = newPos;
+            }
+            if(Input.GetKey(KeyCode.Space))
+            {
+                // Get the current position of the player
+                Vector2 currentPosition = gameObject.transform.position;
+
+                // Get new position by adding to the x coordinate 
+                Vector2 newPos = new Vector2(currentPosition.x, currentPosition.y + jumpHeight);
+
+                //update the player's position
+                gameObject.transform.position = newPos;
+            }
     }
 }
